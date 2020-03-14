be6bf9204ddb8c6c4a597ddf0498c437b37186c7
shivangi-jadon <shivangijadon16@gmail.com>
2020-03-14 07:35:11 +0530

    Update initial.js

--- static/js/initial.js
+++ static/js/initial.js
@@ -1,46 +1,52 @@
-function initial() {
+function initial(){
     $.ajax({
-        type: 'GET',
-        url: '/api/files',
-
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function (data) {
-            //alert('Success!');
-            if (data.length > 0) {
-                document.getElementById("visible").style.display = "block"
-            }
-
-        }
-    });
-}
-//alert("ggg")
-options = {
-    url: '/uploads',
-    acceptedFiles: '.txt,.log',
-    addRemoveLinks: true,
-    dictDefaultMessage: 'Drag a log file here to save, or click to select one',
-    thumbnailWidth: '50px',
-    removedfile: function (file) {
-        file.previewElement.remove();
-    },
-    autoProcessQueue: false,
-    init: function () {
-
-        var myDropzone = this;
-
-        // Update selector to match your button
-        $("#upload").click(function (e) {
-            e.preventDefault();
-            myDropzone.processQueue();
-        });
+                type: 'GET',
+                url: '/api/files',
+                
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function(data) {
+                    //alert('Success!');
+                    if(data.length>0)
+                    {
+                        document.getElementById("visible").style.display="block"
+                        document.getElementById("statistics").setAttribute("onclick","doNav('statistics/"+data[0]+"')")
+                    }
+                   
+                }
+            });
     }
-
-};
-
-const uploader = new Dropzone('#upload-widget', options);
-$("#button").click(function (e) {
-    e.preventDefault();
-    uploader.processQueue();
-});
+    //alert("ggg")
+       options = {
+           url:'/uploads',
+        acceptedFiles: '.txt,.log',
+        addRemoveLinks: true,
+        dictDefaultMessage: 'Drag a log file here to save, or click to select one',
+        thumbnailWidth:'50px',
+        removedfile: function(file) {
+                file.previewElement.remove();
+    },
+        autoProcessQueue: false,
+        init: function () {
+    
+            var myDropzone = this;
+    
+            // Update selector to match your button
+            $("#upload").click(function (e) {
+                e.preventDefault();
+                myDropzone.processQueue();
+                
+            });
+            this.on("queuecomplete", function (file) {
+              alert("ll");
+              initial()
+          });}
+        
+      };
+    
+    const uploader = new Dropzone('#upload-widget', options);
+    $("#button").click(function (e) {
+        e.preventDefault();
+        uploader.processQueue();
+    });
