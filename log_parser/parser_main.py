eac1d4b9fecc1f3f28c808a83131ffa948abf433
sknavilehal <sknavilehal@gmail.com>
2020-05-31 18:44:31 +0530

    time parse fix

--- log_parser/parser_main.py
+++ log_parser/parser_main.py
@@ -8,7 +8,7 @@ def preliminary_run(contents, alerts):
     sig_matches = []
     delimeters = [": //", ": %CVP_", ": %CCBU"]
 
-    time_data = ["", ""]
+    time_data = ["-", "-"]
             
     for content in contents:
         for sig in alerts:
@@ -18,9 +18,11 @@ def preliminary_run(contents, alerts):
             if device == UNKNOWN and delimeter in content:
                 device = device_map[delimeter]
     
-    if device == CVP:
-        time_data[0] = contents[0].split(": ")[2].split('-')[0].strip()
-        time_data[1] = contents[-1].split(": ")[2].split('-')[0].strip()
+    for content in contents:
+        if device == CVP and ": %" in content:
+            if time_data[0] == '-':
+                time_data[0] = content.split(": ")[2].split('-')[0].strip()
+            time_data[1] = content.split(": ")[2].split('-')[0].strip()
     
     alerts = [sig for sig in sig_matches]
     return (alerts, device, time_data)
